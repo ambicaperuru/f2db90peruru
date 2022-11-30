822581cc13cefced9987cc6c73db142f748fee0d
Ambica Peruru <ambicaperuru@gmail.com>
2022-11-29 20:53:26 -0600

    Updated

--- models/mobile.js
+++ models/mobile.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose") 
 const mobileSchema = mongoose.Schema({ 
- mobile_type: String, 
- size: String, 
- cost: Number 
+    mobile_type: {type: String,required: [true, 'Mobile type cannot be empty']},
+    size: {type: String,required: [true, 'Mobile size cannot be empty']},
+    cost: {type: Number,required: [true, 'Mobile cost cannot be empty']},
 }) 
 
 module.exports = mongoose.model("mobiles", mobileSchema) 
--- routes/mobile.js
+++ routes/mobile.js
@@ -1,6 +1,16 @@
 var express = require('express'); 
 const mobile_controlers= require('../controllers/mobile'); 
 var router = express.Router(); 
+// A little function to check if we have an authorized user and continue on 
+//or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
  
 /* GET mobile */ 
 router.get('/', mobile_controlers.mobile_view_all_Page ); 
@@ -11,13 +21,13 @@ router.get('/detail', mobile_controlers.mobile_view_one_Page);
 
 
 /* GET create mobile page */ 
-router.get('/create', mobile_controlers.mobile_create_Page);
+router.get('/create',secured, mobile_controlers.mobile_create_Page);
 
 /* GET create update page */ 
-router.get('/update', mobile_controlers.mobile_update_Page); 
+router.get('/update',secured, mobile_controlers.mobile_update_Page); 
 
 /* GET delete mobile page */ 
-router.get('/delete', mobile_controlers.mobile_delete_Page); 
+router.get('/delete',secured, mobile_controlers.mobile_delete_Page); 
 
 module.exports = router;
  
